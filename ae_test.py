use mse not total loss for plot examples (kl only needed for training) plus small fix to plotting func

ae_test.py
@@ -20,59 +20,64 @@ from mods import LoadData
 
 from torch.cuda.amp import autocast
 
-scaler = StandardScaler()
-loader_of_data = mods.LoadData(scaler, spec_dir ="/home/worrellie/Documents/phd/autoencoder/merged_z09_v3-001")
+# DIR = "/home/worrellie/Documents/phd/autoencoder/merged_z09_v3-001_linearly_scaled_noiseless_1h"
+# DIR = "/home/worrellie/Documents/phd/autoencoder/merged_z09_v3-001_linearly_scaled_noiseless_8h"
+DIR = "/home/worrellie/Documents/phd/autoencoder/merged_z09_v3-001_linearly_scaled_noisy_1h"
+# DIR = "/home/worrellie/Documents/phd/autoencoder/merged_z09_v3-001_linearly_scaled_noisy_8h"
 
-raw_train, raw_valid, raw_test = loader_of_data.load_raw()
-train, valid, test = loader_of_data.scale_raw(raw_train, raw_valid, raw_test)
+scaler = StandardScaler() # initiate scaler for data
+loader_of_data = mods.LoadData(scaler, spec_dir =DIR) # initiate data loader
 
-l = loader_of_data.l
+raw_train, raw_valid, raw_test = loader_of_data.load_raw() # load raw fluxes (returned as np arrays)
+train, valid, test = loader_of_data.scale_raw(raw_train, raw_valid, raw_test) # scale according to scaler used
 
-train_dataset = SpecDataset(train)
+l = loader_of_data.l # get wavelength array
+print(min(l))
+print(max(l))
+exit()
+
+train_dataset = SpecDataset(train) # put scaled fluxes into Pytorch Dataset
 valid_dataset = SpecDataset(valid)
 test_dataset = SpecDataset(test)
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=False)
-valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=32, shuffle=False)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True) # dataset into Pytorch dataloader
+valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=64, shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-TEST_NAME = "all_z09_relu_no_early_stop"
+# intiate test paramters and stuff
 
 SCALING = loader_of_data.scaler
-
 INPUT_SIZE = len(train[1])
-
 CONFIG = [
-    {'in': 5000,   'out': 3000, },
-    {'in': 3000,  'out': 2000, },
+    {'in': 256,   'out': 64, },
 ]
-
-LATENT_SIZE = 500
+LATENT_SIZE = 32
 ACTIVATION_FUNCTION = 'ReLU'
-
-EPOCHS = 500
-
-BETA = 1e-4 # kl weighting only used in VAE
-
+EPOCHS = 50
+EARLY_STOPPING = False
+BETA = 1e-4 # kl weighting only used in VAE, set as 0 for other models
 LEARNING_RATE = 1e-4
 WEIGHT_DECAY = 1e-8
 
+TEST_NAME = "report_specifications"
+# TEST_NAME = f'z09_1h_{ACTIVATION_FUNCTION}_{EPOCHS}e_es{EARLY_STOPPING}_ls{LATENT_SIZE}'
+
 test_params = {
     'test_name': TEST_NAME,
+    'data_dir': DIR,
     'scaling' : SCALING,
     'config' : CONFIG,
     'latent_size' : LATENT_SIZE,
     'activation_function' : ACTIVATION_FUNCTION,
     'epochs' : EPOCHS,
     'beta' : BETA,
-    'lr' : LEARNING_RATE,
+    'learn_rate' : LEARNING_RATE,
     'weight_decay' : WEIGHT_DECAY
 }
 
+TESTING = False
 
-testing = False
-
-funcs.save_test_params(test_params, TEST_NAME, test=testing)
+funcs.save_test_params(test_params, TEST_NAME, test=TESTING)
 
 ###############################################
 
@@ -83,7 +88,10 @@ print(model)
 # print(funcs.get_model_size_mb(model))
 # exit()
 
-# early_stopping = mods.CustomEarlyStopping(TEST_NAME, patience = 10, delta = 0.0, test = testing, verbose = True)
+if EARLY_STOPPING:
+    early_stopping = mods.CustomEarlyStopping(TEST_NAME, patience = 10, delta = 0.0, test = testing, verbose = True)
+else:
+    early_stopping = None
 
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
 # optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
@@ -92,12 +100,13 @@ optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT
 
 # train
 torch.cuda.empty_cache()
-model, model_losses = train_ae(EPOCHS, train_loader, test_loader, model, optimizer, beta=BETA, verbose = True, )
+model, model_losses = train_ae(EPOCHS, train_loader, test_loader, model, optimizer, early_stopping = early_stopping, beta=BETA, verbose = True, )
 
-funcs.plot_loss(model_losses, test_params['test_name'], test=testing)
+funcs.plot_loss(model_losses, test_params['test_name'], test=TESTING)
 
 MU = loader_of_data.scaler
 SIGMA = loader_of_data.scaler
 
-funcs.plot_examples(train_loader, model, l, test_params, SCALING, test = testing)
+funcs.plot_examples(train_loader, model, l, test_params, SCALING, test = TESTING)
+# funcs.plot_examples(valid_loader, model, l, test_params, SCALING, test = TESTING)
 

funcs.py
@@ -34,8 +34,6 @@ def get_model_size_mb(model):
 
     return total_size_mb
 
-
-
 def _loss_calc(x_hat, x, mu = None, logvar = None, beta = 0, red = 'mean'):
 
     mse = nn.MSELoss(reduction = red)
@@ -259,19 +257,31 @@ def test_agn(loader, model):
 
 def _get_example_specs(loader, model):
 
+    # temp loader to not shuffle data (so can get the right pairs)
+    temp_loader = torch.utils.data.DataLoader(
+        loader.dataset, 
+        batch_size=loader.batch_size, 
+        shuffle=False, 
+    )
+
     print('predicting...')
     losses = []
+    model.eval()
     with torch.no_grad():
         for x, _ in loader:
             # print(x.size())
             x = x.to(device)
             x_hat, mu, logvar = model(x)
-            loss, mse, kl = _loss_calc(x_hat, x, mu, logvar, red='none')
-            loss = loss.detach().cpu()
-            # print(loss.shape)
-            loss_per_spec = loss.mean(dim=1) # mean per spec
+            mse, _, _ = _loss_calc(x_hat, x, mu, logvar, red='none')
+            print(mse)
+            mse = mse.detach().cpu()
+            # print('HERE ', mse.shape)
+            # loss_per_spec = mse.mean(dim=1) # mean per spec
+            loss_per_spec = mse.mean() # mean per spec
             # print(loss_per_spec.shape)
-            losses.extend(loss_per_spec)
+            # losses.extend(loss_per_spec.cpu().numpy().tolist())
+            # losses.extend(loss_per_spec.numpy())
+            losses.append(loss_per_spec.numpy())
 
     to_find = {
         "min": np.min(losses),
@@ -302,6 +312,7 @@ def _predict_examples(dataset, indices, model):
 
     print('predicting min, max, mean and quartiles...')
 
+    model.eval()
     with torch.no_grad():
         for i in indices:
             x = dataset[i][0].to(device)
@@ -311,18 +322,22 @@ def _predict_examples(dataset, indices, model):
             # note: need batch dimension for model
             x_hat, mu, logvar = model(x)
             # print(x_hat.shape)
-            loss, mse, kl = _loss_calc(x_hat, x, mu, logvar, red='none')
-            loss = loss.detach().cpu()
-            # print(loss.shape)
-            loss_per_spec = loss.mean(dim=1) # mean per spec
+            mse, _, _ = _loss_calc(x_hat, x, mu, logvar, red='none')
+            mse = mse.detach().cpu()
+            # print('here also', mse.shape)
+            # loss_per_spec = mse.mean(dim=1) # mean per spec
+            loss_per_spec = mse.mean() # mean per spec
             # print(loss_per_spec.shape)
-            output['loss'].extend(loss_per_spec)
+            # output['loss'].extend(loss_per_spec.cpu().numpy().tolist())
+            # output['loss'].extend(loss_per_spec.numpy())
+            output['loss'].append(loss_per_spec.numpy())
 
             x = x.squeeze(0)
             x_hat = x_hat.squeeze(0) # add then remove batch dimension
             x_hat = x_hat.detach().cpu()
             output['recon'].append(x_hat.tolist())
             output['original'].append(x.tolist())
+            print(len(output['loss']))
 
 
     return output
@@ -386,7 +401,8 @@ def _draw_spec_pair(ax_fit, ax_res, output, i, l, scaler):
     og = og.flatten('F')
     
     
-    resid = [x - y for x,y in zip(output['original'][i], output['recon'][i])]
+    # resid = [x - y for x,y in zip(output['original'][i], output['recon'][i])]
+    resid = og - recon
     # r2 =[np.pow(x, 2) for x in resid]
     # mean_r2 = np.mean(r2)
 
@@ -434,4 +450,15 @@ def save_test_params(test_dict, test_name, test=False):
     with open(path_name, 'wb') as p:
         pkl.dump(test_dict, p)
 
+def global_stats(loader):
+
+    all_fluxes = []
+    for batch_flux, batch_mask in loader:
+
+        mask = batch_flux != 0
+        all_fluxes.append(batch_flux[mask])
+
+    combined_fluxes = torch.cat(all_fluxes)
+    
+    return combined_fluxes.mean(), combined_fluxes.std()
 

mods.py
@@ -11,9 +11,36 @@ import torch.nn.functional as F
 from torch.distributions.normal import Normal
 import math
 import pathlib as path
+import h5py
 
 import funcs
 import mods
+
+class H5SpecDataset(torch.utils.data.Dataset):
+
+    def __init__(self, data_path, split = 'train', flux_type = 'normalized_flux'):
+        self.data_path = data_path
+        self.split = split
+        self.flux_type = flux_type
+
+        with h5py.File(self.data_path, 'r') as hf:
+            self.len = hf[self.split][self.flux_type].shape[0]
+            # print(self.len)
+            # print(len(hf[self.split][self.flux_type]))
+            self.n_pixels = hf[self.split][self.flux_type].shape[1]
+            # print(self.n_pixels)
+
+    def __len__(self):
+
+        return self.len
+    
+    def __getitem__(self, idx):
+        with h5py.File(self.data_path, 'r') as hf:
+            sample = torch.from_numpy(hf[self.split][self.flux_type][idx])
+            sample_mask = sample != 0
+            print(sample_mask)
+            return sample, sample_mask
+
 
 class SpecDataset(torch.utils.data.Dataset):
 
@@ -30,6 +57,57 @@ class SpecDataset(torch.utils.data.Dataset):
         return sample, sample
 
 ####### autoencoders ########
+
+
+class ReportAutoencoder(nn.Module):
+
+    def __init__(self, input_size, activation = 'ReLU'):
+        super(ReportAutoencoder, self).__init__()
+
+        self.act_func = getattr(nn, activation)() # make instance of desired activation function
+
+
+
+        self.layers = nn.ModuleList()
+
+        self.input_to_encoder = nn.Linear(input_size, 256)
+        self.layers.append(self.input_to_encoder)
+
+        self.encoder_to_latent = nn.Linear(256, 64)
+        self.layers.append(self.encoder_to_latent)
+        
+        self.latent_to_decoder = nn.Linear(64, 256)
+        self.layers.append(self.latent_to_decoder)
+
+        self.decoder_to_output = nn.Linear(64, input_size)
+        self.layers.append(self.decoder_to_output)
+
+    def forward(self, x):
+
+        x = self.act_func(self.input_to_encoder(x))
+        x = self.act_func(self.encoder_to_latent(x))
+
+        x = self.act_func(self.latent_to_decoder(x))
+        x_hat = self.decoder_to_output(x)
+
+        # for l in self.layers:
+        #     # x = torch.relu(l(x))
+        #     x = self.act_func(l(x))
+
+        # z = torch.relu(self.encoder_to_latent(x))
+        # z = self.act_func(self.encoder_to_latent(x))
+
+        # z = torch.relu(self.decoder_from_latent(z))
+        # z = self.act_func(self.decoder_from_latent(z))
+
+        # for l in self.decoder_layers:
+        #     # z = torch.relu(l(z))
+        #     z = self.act_func(l(z))
+
+        x_hat = self.decoder_to_output(z)
+
+        return x_hat, None, None
+
 
 class StandardAutoencoder(nn.Module):
 
@@ -251,70 +329,70 @@ class CNNAutoencoder(nn.Module):
 
         return x_hat, mu, logvar
 
-class LoadData:
+# class LoadData:
 
-    def __init__(self, scaler, spec_dir ="/home/worrellie/Documents/phd/autoencoder/merged_spectra_gal",
-                 agn_dir="/home/worrellie/Documents/phd/autoencoder/agn/merged_spectra_agn"):
+#     def __init__(self, scaler, spec_dir ="/home/worrellie/Documents/phd/autoencoder/merged_spectra_gal",
+#                  agn_dir="/home/worrellie/Documents/phd/autoencoder/agn/merged_spectra_agn"):
         
-        self.spec_dir = spec_dir
-        self.agn_dir = agn_dir
-        self.scaler = scaler
+#         self.spec_dir = spec_dir
+#         self.agn_dir = agn_dir
+#         self.scaler = scaler
 
-    def load_raw(self,):
+#     def load_raw(self,):
 
-        fluxes = []
-        i=0
-        for spec in os.listdir(self.spec_dir):
-            i= i+1
-            spec_path = os.path.join(self.spec_dir, spec)
-            try:
-                with fits.open(spec_path) as hdul:
+#         fluxes = []
+#         i=0
+#         for spec in os.listdir(self.spec_dir):
+#             i= i+1
+#             spec_path = os.path.join(self.spec_dir, spec)
+#             try:
+#                 with fits.open(spec_path) as hdul:
 
-                    data = hdul[1].data
-                    flux = data['flux']
-                    # print(flux)
-                    l = data['lambda']
-                    self.l = l
-                    flux = flux.astype(np.float32)
-                    flux = torch.from_numpy(flux)
-                    fluxes.append(flux)
-                    # if i == 1:
-                    #     plt.figure()
-                    #     plt.plot(l,flux)
-                    #     # plt.show()
+#                     data = hdul[1].data
+#                     flux = data['flux']
+#                     # print(flux)
+#                     l = data['lambda']
+#                     self.l = l
+#                     flux = flux.astype(np.float32)
+#                     flux = torch.from_numpy(flux)
+#                     fluxes.append(flux)
+#                     # if i == 1:
+#                     #     plt.figure()
+#                     #     plt.plot(l,flux)
+#                     #     # plt.show()
         
 
-            except Exception as e:
-                print(f"Error opening spectrum: {spec} ({e})")
+#             except Exception as e:
+#                 print(f"Error opening spectrum: {spec} ({e})")
             
 
-        fluxes = np.asarray(fluxes)
-        INPUT_SIZE = len(fluxes[1])
+#         fluxes = np.asarray(fluxes)
+#         INPUT_SIZE = len(fluxes[1])
 
-        f_train, f_test = train_test_split(fluxes)
-        f_train, f_valid = train_test_split(f_train, test_size = 0.1)
+#         f_train, f_test = train_test_split(fluxes)
+#         f_train, f_valid = train_test_split(f_train, test_size = 0.1)
 
-        return f_train, f_valid, f_test
+#         return f_train, f_valid, f_test
 
-    def scale_raw(self, f_train, f_valid, f_test):
+#     def scale_raw(self, f_train, f_valid, f_test):
         
-        f_train = np.asarray(f_train)
-        f_valid = np.asarray(f_valid)
-        f_test = np.asarray(f_test)
+#         f_train = np.asarray(f_train)
+#         f_valid = np.asarray(f_valid)
+#         f_test = np.asarray(f_test)
 
-        self.scaler = self.scaler.fit(f_train)
+#         self.scaler = self.scaler.fit(f_train)
 
-        scaled_train = self.scaler.transform(f_train)
-        scaled_valid = self.scaler.transform(f_valid)
-        scaled_test = self.scaler.transform(f_test)
+#         scaled_train = self.scaler.transform(f_train)
+#         scaled_valid = self.scaler.transform(f_valid)
+#         scaled_test = self.scaler.transform(f_test)
 
-        # convert to pytorch tensor
+#         # convert to pytorch tensor
 
-        scaled_train = torch.from_numpy(scaled_train)
-        scaled_valid = torch.from_numpy(scaled_valid)
-        scaled_test = torch.from_numpy(scaled_test)
+#         scaled_train = torch.from_numpy(scaled_train)
+#         scaled_valid = torch.from_numpy(scaled_valid)
+#         scaled_test = torch.from_numpy(scaled_test)
 
-        return scaled_train, scaled_valid, scaled_test
+#         return scaled_train, scaled_valid, scaled_test
 
     # def load_agn(self,):
 

process_specs_gal.py
@@ -2,89 +2,168 @@ import os
 import glob
 import numpy as np
 from astropy.io import fits
+import re
+import joblib
+
+from matplotlib import pyplot as plt
+
+from specutils import Spectrum
+import astropy.units as u
+from specutils.manipulation import FluxConservingResampler
+
+from specutils import SpectralRegion
 
 #####################
-input_dir = '/home/worrellie/Documents/phd/autoencoder/Datasets/z09_v3-001'
+input_dir = '/home/worrellie/Documents/phd/autoencoder/z09_and_z08'
+t = 1  # 1: noisy, 4: template
+lin_scale = False
+exps = [1, 2, 4, 8]
 
-t = 1 # noisy spec
-# t = 4 # template spec
-
-# exp_time = "1h"
-exp_time = "8h"
-
-lin_scale = True
-#####################
-
-if t == 1 and lin_scale:
-    output_dir = f'merged_z09_v3-001_linearly_scaled_noisy_{exp_time}'
-elif t == 1 and not lin_scale:
-    output_dir = f'merged_z09_v3-001_noisy_{exp_time}'
-if t == 4 and lin_scale:
-    output_dir = f'merged_z09_v3-001_linearly_scaled_noiseless_{exp_time}'
-elif t == 4 and not lin_scale:
-    output_dir = f'merged_z09_v3-001_noiseless_{exp_time}'
-
+# Define output directory
+suffix = "linearly_scaled_" if lin_scale else ""
+noise_type = "noisy" if t == 1 else "noiseless"
+output_dir = f'processed_z09_z08_normalized_{suffix}{noise_type}'
 os.makedirs(output_dir, exist_ok=True)
 
-# 1. Identify all unique '1h' exposure IDs
-# We look for the RI files as a reference point for each unique object
-pattern = os.path.join(input_dir, f"*_{exp_time}_RI.fits")
-ri_files = glob.glob(pattern)
+all_rest_mins = []
+all_rest_maxs = []
+valid_file_triplets = []
 
-for ri_path in ri_files:
-    # Extract the base name (e.g., mambo_97000060017188_z0.9035)
-    base_name = os.path.basename(ri_path).replace('_RI.fits', '')
-    
-    # Define the paths for the 3 parts
-    parts = {
-        'RI': ri_path,
-        'YJ': ri_path.replace('_RI.fits', '_YJ.fits'),
-        'H':  ri_path.replace('_RI.fits', '_H.fits')
-    }
-    
-    # Check if all 3 parts actually exist
-    if not all(os.path.exists(p) for p in parts.values()):
-        print(f"Skipping {base_name}: Missing one or more arms.")
-        continue
+for exp_time in exps:
+    pattern = os.path.join(input_dir, f"*_{exp_time}h_RI.fits")
+    ri_files = glob.glob(pattern)
 
+    for ri_path in ri_files:
+        # Match redshift from filename
+        match = re.search(r'z(\d+\.\d+)', ri_path)
+        if not match: continue
+        z = float(match.group(1))
+
+        # Check for all channels
+        yj_path = ri_path.replace('_RI.fits', '_YJ.fits')
+        h_path = ri_path.replace('_RI.fits', '_H.fits')
+        
+        if os.path.exists(yj_path) and os.path.exists(h_path):
+            # Speed fix: Use fits.getdata to only grab the wavelength extension (idx 9)
+            # This is much faster than fits.open() which maps the whole file
+            try:
+                wav_ri = fits.getdata(ri_path, ext=9)
+                wav_h = fits.getdata(h_path, ext=9)
+                
+                obs_min = wav_ri.min()
+                obs_max = wav_h.max()
+                
+                all_rest_mins.append(obs_min / (1 + z))
+                all_rest_maxs.append(obs_max / (1 + z))
+                valid_file_triplets.append((ri_path, yj_path, h_path, z))
+            except Exception as e:
+                print(f"Error reading {ri_path}: {e}")
+
+
+# Determine the common overlap (Inner envelope)
+common_min = np.max(all_rest_mins)
+common_max = np.min(all_rest_maxs)
+# Define the common grid (e.g., 1.0 Angstrom spacing)
+common_grid = np.arange(np.ceil(common_min), np.floor(common_max), 1.0) * u.AA
+
+print(f"✅ Common grid found: {common_min:.2f} to {common_max:.2f} Å")
+print(f"Total files to process: {len(valid_file_triplets)}")
+
+
+##
+
+resampler = FluxConservingResampler()
+
+for ri_p, yj_p, h_p, redshift in valid_file_triplets:
+    base_name = os.path.basename(ri_p).replace('_RI.fits', '')
+    
     combined_lambda = []
     combined_flux = []
+    bounds = [] # for instrument gap
 
-    # 2. Extract data from each part
-    # Order matters: RI (Blue) -> YJ (Green) -> H (Red)
-    for arm in ['RI', 'YJ', 'H']:
-        with fits.open(parts[arm]) as hdul:
+    # Load channels
+    for p in [ri_p, yj_p, h_p]:
+        with fits.open(p) as hdul:
             flux = hdul[t].data
-            flux = flux*1e19
-            l = hdul[9].data # wavelength
-            # Assuming columns are named 'lambda' and 'flux'
-            # Adjust these strings if your FITS headers use different names
-            combined_lambda.append(l)
+            if lin_scale: flux *= 1e19
+            wav = hdul[9].data
+            combined_lambda.append(wav)
             combined_flux.append(flux)
+            bounds.append((wav.min(), wav.max()))
 
-
-    # 3. Concatenate and sort
-    # Even if files are read in order, it's safer to sort by wavelength
-    final_lambda = np.concatenate(combined_lambda)
-    final_flux = np.concatenate(combined_flux)
-    
+    # Flatten and Sort
+    final_lambda = np.concatenate(combined_lambda) * u.AA
+    final_flux = np.concatenate(combined_flux) * u.Unit('erg cm-2 s-1 AA-1')
     sort_idx = np.argsort(final_lambda)
-    final_lambda = final_lambda[sort_idx]
-    final_flux = final_flux[sort_idx]
-
-    # 4. Save to a new FITS file
-    col1 = fits.Column(name='lambda', format='D', array=final_lambda)
-    col2 = fits.Column(name='flux', format='D', array=final_flux)
-    hdu = fits.BinTableHDU.from_columns([col1, col2])
     
-    if t == 1 and lin_scale:
-        output_filename = f"{base_name}_linearly_scaled_noisy.fits"
-    elif t == 1 and not lin_scale:
-        output_filename = f"{base_name}_noisy.fits"
-    if t == 4 and lin_scale:
-        output_filename = f"{base_name}_linearly_scaled_noiseless.fits"
-    elif t == 4 and not lin_scale:
-        output_filename = f"{base_name}_noiseless.fits"
+    # Create Spectrum1D in rest-frame
+    # lambda_rest = lambda_obs / (1+z)
+    spec_rest = Spectrum(
+        spectral_axis=(final_lambda[sort_idx] / (1 + redshift)),
+        flux=final_flux[sort_idx]
+    )
 
-    hdu.writeto(os.path.join(output_dir, output_filename), overwrite=True)
-    print(f"Created: {output_filename}")
+    # REBIN to common grid
+    rebinned_spec = resampler(spec_rest, common_grid)
+    flux_vals = rebinned_spec.flux.value # Convert to raw numpy array
+
+    # Create a mask for intrument gap: Start with all False (gap)
+    valid_mask = np.zeros(len(common_grid), dtype=bool)
+    for obs_min, obs_max in bounds:
+        # De-redshift the boundaries of each arm
+        rest_min = obs_min / (1 + redshift)
+        rest_max = obs_max / (1 + redshift)
+        
+        # Mark pixels in the common_grid that fall within a real detector arm
+        valid_mask |= (common_grid.value >= rest_min) & (common_grid.value <= rest_max)
+
+    # Set everything outside the arms (the gaps) to 0.0
+    flux_vals[~valid_mask] = 0.0
+    # ---------------------------------
+
+    # 4. INDIVIDUAL NORMALIZATION
+    # We use the median of the non-zero pixels
+    nonzero_pixels = flux_vals[valid_mask]
+    norm_factor = 1.0
+    if len(nonzero_pixels) > 0:
+        norm_factor = np.median(nonzero_pixels)
+        # Avoid division by zero if median is 0
+        if norm_factor != 0:
+            flux_vals = flux_vals / norm_factor
+
+
+    # Update the plot to show the enforced gaps
+    if base_name == valid_file_triplets[0][0].split('/')[-1].replace('_RI.fits', ''):
+        plt.figure(figsize=(12, 6))
+        plt.step(spec_rest.spectral_axis, spec_rest.flux, 
+                where='mid', color='gray', alpha=0.3, label='Input (Rest)')
+        plt.step(common_grid, flux_vals, 
+                where='mid', color='crimson', lw=1.5, label='Rebinned & Masked')
+        
+        # Highlight where the gaps were enforced
+        plt.fill_between(common_grid.value, 0, np.nanmax(flux_vals), 
+                         where=~valid_mask, color='black', alpha=0.1, label='Enforced Gap')
+        
+        plt.title(f"Verification: {base_name} (z={redshift})")
+        plt.legend()
+        plt.savefig(os.path.join(output_dir, f"{base_name}_check.png"))
+        plt.show()
+        plt.close()
+
+    # --- SAVE TO FITS ---
+    col1 = fits.Column(name='lambda', format='D', array=common_grid.value)
+    col2 = fits.Column(name='flux', format='D', array=flux_vals) # Use the masked flux
+    
+    hdr = fits.Header()
+    hdr['REDSHIFT'] = redshift
+    hdr['ORIGINAL'] = base_name
+    hdr['NORMFAC'] = norm_factor # Important to keep for science!
+    
+    hdu = fits.BinTableHDU.from_columns([col1, col2], header=hdr)
+    
+    out_name = f"{base_name}_{noise_type}_rebinned.fits"
+    if lin_scale: out_name = out_name.replace('_rebinned', '_scaled_rebinned')
+    
+    hdu.writeto(os.path.join(output_dir, out_name), overwrite=True)
+
+