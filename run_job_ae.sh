796e211b0e0901df941faf07e5dffd02ad955c1e
worrellie <eleanor.worrell@btinternet.com>
2026-04-29 15:34:36 +0100

    update sbatch

--- run_job_ae.sh
+++ run_job_ae.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name=run_ae
-#SBATCH --output=/home/vboyanov/ml_out/run_ae-%j.out
-#SBATCH --error=/home/vboyanov/ml_out/run_ae-%j.err
+#SBATCH --job-name=run_sae
+#SBATCH --output=/home/vboyanov/ml_out/run_ae-%A_%a.out
+#SBATCH --error=/home/vboyanov/ml_out/run_ae-%A_%a.err
 #SBATCH --mail-user=valentinboyanov@tecnico.ulisboa.pt
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
-#SBATCH --mem=32G
+#SBATCH --mem=8G
 #SBATCH --time=04:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
+#SBATCH --array=0-2
 
 RUNPATH=/home/vboyanov/ml
 cd $RUNPATH
@@ -18,4 +19,4 @@ source $RUNPATH/bt_env/bin/activate
 
 export SLURM_CPUS_PER_TASK=8
 
-python -u run_ae.py
+python -u run_ae.py --task_id $SLURM_ARRAY_TASK_ID
