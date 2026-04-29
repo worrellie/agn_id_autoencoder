8fe0231059689f9ae711683a48872ef58a9c5653
worrellie <eleanor.worrell@btinternet.com>
2026-04-29 15:38:45 +0100

    update sbatch

--- run_job_ae.sh
+++ run_job_ae.sh
@@ -12,11 +12,13 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --array=0-2
 
-RUNPATH=/home/vboyanov/ml
+RUNPATH=/home/vboyanov/ml/
 cd $RUNPATH
 
 source $RUNPATH/bt_env/bin/activate
 
 export SLURM_CPUS_PER_TASK=8
+
+cd $RUNPATH/autoencoder/
 
 python -u run_ae.py --task_id $SLURM_ARRAY_TASK_ID
